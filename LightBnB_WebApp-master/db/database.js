00db88e9f623563ab1554fa071afc7ef859f72b2
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-01-22 14:44:37 -0500

    Project structure refactor

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,15 +1,4 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
-
+const db = require('./index');
 
 /// Users
 
@@ -19,14 +8,18 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  return pool
+  return db
     .query(`
-    SELECT id, name, email, password 
+    SELECT users.*
     FROM users 
     WHERE email = $1
-    `, [email])
-    .then((result) => {
-      return result.rows[0];
+    `, [email.toLowerCase()])
+    .then(res =>  {
+      if (!res) {
+        return null;
+      } else {
+        return res.rows[0];
+      }
     })
     .catch((err) => {
       console.log(err.message);
@@ -39,14 +32,18 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool
+  return db
     .query(`
-    SELECT id, name, email, password 
+    SELECT users.*
     FROM users 
     WHERE id = $1
     `, [id])
-    .then((result) => {
-      return result.rows[0];
+    .then(res =>  {
+      if (!res) {
+        return null;
+      } else {
+        return res.rows[0];
+      }
     })
     .catch((err) => {
       console.log(err.message);
@@ -59,14 +56,18 @@ const getUserWithId = function(id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  return pool
+  return db
     .query(`
     INSERT INTO users (name, email, password)
     VALUES ($1, $2, $3) 
     RETURNING *;
     `, [user.name, user.email, user.password])
-    .then((result) => {
-      return result.rows[0];
+    .then(res =>  {
+      if (!res) {
+        return null;
+      } else {
+        return res.rows[0];
+      }
     })
     .catch((err) => {
       console.log(err.message);
@@ -81,7 +82,7 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
+  return db
     .query(`SELECT reservations.id, properties.*, reservations.start_date, reservations.end_date, AVG(rating) AS average_rating
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
@@ -90,8 +91,12 @@ const getAllReservations = function (guest_id, limit = 10) {
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
     LIMIT $2;`, [guest_id, limit])
-    .then((result) => {
-      return result.rows;
+    .then(res =>  {
+      if (!res) {
+        return null;
+      } else {
+        return res.rows;
+      }
     })
     .catch((err) => {
       console.log(err.message);
@@ -115,7 +120,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   //Check if a city has been passed in as an option
@@ -166,7 +171,7 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
   //Run the query
-  return pool.query(queryString, queryParams)
+  return db.query(queryString, queryParams)
     .then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
@@ -179,14 +184,18 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  return pool
+  return db
     .query(`
     INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
     `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property. number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
-    .then((result) => {
-      return result.rows[0];
+    .then(res =>  {
+      if (!res) {
+        return null;
+      } else {
+        return res.rows[0];
+      }
     })
     .catch((err) => {
       console.log(err.message);
